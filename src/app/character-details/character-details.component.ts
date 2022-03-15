a83a0c56775635a98fb5de13af691e304510a3a5
Aidon Jardim <50612642+ajar1000@users.noreply.github.com>
2022-03-15 21:59:37 +1100

    Get details working
    Started 8:30pm
    * Fixed graphql details query

--- src/app/character-details/character-details.component.ts
+++ src/app/character-details/character-details.component.ts
@@ -1,6 +1,17 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Apollo, gql } from 'apollo-angular';
+
+interface Character {
+  id: string;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  created: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-character-details',
@@ -9,6 +20,9 @@ import { Apollo, gql } from 'apollo-angular';
 })
 export class CharacterDetailsComponent {
   id?: string;
+  character?: Character;
+  loading: boolean = true;
+
   constructor( 
     private router: ActivatedRoute,
     private apollo: Apollo,
@@ -16,12 +30,13 @@ export class CharacterDetailsComponent {
   {
     this.router.params.subscribe(param => {
       this.id = param["id"];
+      this.loading = true;
 
       this.apollo
         .query({
           query: gql`
-          query getCharacter($id: String!) {
-            charactersByIds(ids: [$id]) {
+          query getCharacter($id: [ID!]!) {
+            charactersByIds(ids: $id) {
               id
               name
               status
@@ -53,10 +68,24 @@ export class CharacterDetailsComponent {
           }
           `,
           variables: {
-            id: "3"
+            id: [this.id]
           }
         })
-        .subscribe( s => console.log( s ))
+        .subscribe( s => {
+          console.log( s );
+          const charactersByIds: any[] = (s.data as any).charactersByIds;
+          this.character = charactersByIds.map( c => ({
+            id: c.id,
+            name: c.name,
+            status: c.status,
+            species: c.species,
+            type: c.type,
+            gender: c.gender,
+            created: c.created,
+            image: c.image,
+          }))[0];
+          this.loading = false;
+        })
       });
   }
 }
